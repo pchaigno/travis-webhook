bee62ea8c615791d78347fa24086cd74a91b0d09
Paul Chaignon <paul.chaignon@insa-rennes.fr>
2014-04-02 11:12:46 +0200

    Comments added

--- travis.php
+++ travis.php
@@ -1,8 +1,10 @@
 <?php
 
-define('TRAVIS_TOKEN', file_get_contents('/var/www/travis/travis_info'));
+define('TRAVIS_TOKEN', getTravisToken());
 define('REPOSITORY', 'pchaigno/ProjetVaR');
+define('OWNER_EMAIL', 'paul.chaignon@gmail.com');
 
+// Exists if the sender is not Travis:
 if(!checkTravisAuthorization()) {
 	notifyAdmin();
 	header('401 Not Authorized');
@@ -10,9 +12,13 @@ if(!checkTravisAuthorization()) {
 }
 
 if(isset($_POST['payload'])) {
+	// Logs the JSON document.
 	file_put_contents('notify.json', $_POST['payload']);
+	
+	/**
+	 * Reads all information from the JSON document.
+	 */
 	$json = json_decode($_POST['payload']);
-
 	$repository = $json->{'repository'}->{'owner_name'}.'/'.$json->{'repository'}->{'name'};
 	$repository_url = $json->{'repository'}->{'url'};
 	$branch = $json->{'branch'};
@@ -31,6 +37,9 @@ if(isset($_POST['payload'])) {
 	$duration_text = date('i', $duration).' minutes and '.date('s', $duration).' seconds';
 	$title = '[Broken] '.$repository.'#'.$build_number.' ('.$branch.' - '.$commit.')';
 
+	/**
+	 * Reads the HTML text and writes all the variables.
+	 */
 	$html = file_get_contents('notify.html');
 	$html = str_replace('##repository##', str_replace('/', ' / ', $repository), $html);
 	$html = str_replace('##repository_url##', $repository_url, $html);
@@ -44,6 +53,8 @@ if(isset($_POST['payload'])) {
 	$html = str_replace('##duration##', $duration_text, $html);
 	$html = str_replace('##compare_url##', $compare_url, $html);
 	$html = str_replace('##commit_url##', $commit_url, $html);
+
+	// Sends the mail:
 	if(sendMail($committer_email, $title, $html)) {
 		echo 'Mail sent!';
 	} else {
@@ -51,6 +62,12 @@ if(isset($_POST['payload'])) {
 	}
 }
 
+/**
+ * Convert a Travis date into a timestamp.
+ * Exists if the date is incorrect.
+ * @param date_formatted The date in the Travis format.
+ * @return The date as a timestamp.
+ */
 function getTimestamp($date_formatted) {
 	$format = 'Y-m-d h: i: s';
 	$date_formatted = str_replace(array('T', 'Z'), array(' ', ''), $date_formatted);
@@ -61,6 +78,11 @@ function getTimestamp($date_formatted) {
 	return $date->getTimestamp();
 }
 
+/**
+ * Checks that the sender of the request is Travis.
+ * If he is he should have sent a hash (with the Travis token) in the headers.
+ * @return True if the sender is Travis.
+ */
 function checkTravisAuthorization() {
 	$headers = apache_request_headers();
 	if(isset($headers['Authorization']) {
@@ -73,7 +95,12 @@ function checkTravisAuthorization() {
 	return false;
 }
 
-function notifyAdmin($ipAddress) {
+/**
+ * Notifies the owner/admin.
+ * It sends the IP address of the "attacker".
+ * If a payload was sent as POST content, it is also communicated.
+ */
+function notifyAdmin() {
 	$message = 'Hi master,<br/><br/>';
 	$message .= 'Someone tried to impersonate Travis.<br/>';
 	$message .= 'His IP address is: '.$_SERVER['REMOTE_ADDR'].'.<br/>';
@@ -84,9 +111,17 @@ function notifyAdmin($ipAddress) {
 		$message .= '</code>';
 	}
 	$message .= '<br/><br/>Travis';
-	sendMail('paul.chaignon@gmail.com', 'Unauthorized request on WebHook', $message);
+	sendMail(OWNER_EMAIL, 'Unauthorized request on WebHook', $message);
 }
 
+/**
+ * Sends an email using PHPMailer.
+ * Note: The owner is always in the recipients.
+ * @param recipient The recipient for the email.
+ * @param subject The subject of the email.
+ * @param message An HTML text to be displayed as the message.
+ * @return True if the email was successfully sent.
+ */
 function sendMail($recipient, $subject, $message) {
 	require_once('PHPMailer/class.phpmailer.php');
 
@@ -101,8 +136,10 @@ function sendMail($recipient, $subject, $message) {
 	$mail->SMTPSecure = 'tls';
 
 	$mail->SetFrom('pchaigno@insa-rennes.fr', 'Travis CI');
-	$mail->addAddress($recipient);
-	$mail->addAddress('paul.chaignon@gmail.com');
+	if($recipient != OWNER_EMAIL) {
+		$mail->addAddress($recipient);
+	}
+	$mail->addAddress(OWNER_EMAIL);
 
 	$mail->WordWrap = 50;
 	$mail->isHTML(true);
@@ -113,6 +150,19 @@ function sendMail($recipient, $subject, $message) {
 	return $mail->send();
 }
 
+/**
+ * Retrieves the token for Travis.
+ * @return The Travis token.
+ */
+function getTravisToken() {
+	$token = file_get_contents('/var/www/travis/travis_info');
+	return trim($token);
+}
+
+/**
+ * Retrieves the password for SMTP.
+ * @return The SMTP password.
+ */
 function getSMTPPassword() {
 	$password = file_get_contents('/var/www/travis/smtp_info');
 	return trim($password);
